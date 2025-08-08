16c462e2db2d99b071fdadc592f641dde72eb89d
liuyanyan0321 <44769772+liuyanyan0321@users.noreply.github.com>
2025-08-08 19:09:43 +0900

    Revert "fix;1回目"

--- test.php
+++ test.php
@@ -12,30 +12,4 @@ if($resulr===$row){
 else{
     echo "違う";
 }
-$array=[];
-for($i=0;$i<3;$i++){
-    $array[$i]=rand(1,9);
-}
-$resulr=[1,2,4];
-$row=[1,2,3];
-if($resulr===$row){
-    echo "同じ";
-}
-else{
-    echo "違う";
-}
-
-// 2回目
-$array=[];
-for($i=0;$i<3;$i++){
-    $array[$i]=rand(1,9);
-}
-$resulr=[1,2,4];
-$row=[1,2,3];
-if($resulr===$row){
-    echo "同じ";
-}
-else{
-    echo "違う";
-}
 ?>
